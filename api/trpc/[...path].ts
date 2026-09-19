9b66848f671979070b590cf0bd0ba97a94debe66
hlkhualianAI <hlkhualian@gmail.com>
2026-09-19 05:25:17 +0000

    fix: route all tRPC procedures on Vercel

--- api/trpc/[...path].ts
+++ api/trpc/[...path].ts
@@ -1,6 +1,6 @@
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { appRouter } from "../server/routers";
-import { createContext } from "../server/_core/context";
+import { appRouter } from "../../server/routers";
+import { createContext } from "../../server/_core/context";
 
 export default createExpressMiddleware({
   router: appRouter,
