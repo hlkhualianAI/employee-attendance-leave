5379ed32fe6f058b75ef245e716da9b85c0fe029
hlkhualianAI <hlkhualian@gmail.com>
2026-09-19 05:20:10 +0000

    fix: expose Vercel API routes directly

---
+++ api/trpc.ts
@@ -0,0 +1,8 @@
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { appRouter } from "../server/routers";
+import { createContext } from "../server/_core/context";
+
+export default createExpressMiddleware({
+  router: appRouter,
+  createContext,
+});
